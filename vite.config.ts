1afaf658c9cf439c8e52f9ad36477d76112e7b75
张见飞 <zhangjianfei@zhangjianfeideMacBook-Pro.local>
2021-08-19 18:33:32 +0800

    antd-mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import createImportPlugin from 'vite-plugin-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    createImportPlugin([
+      {
+        libraryName: 'antd',
+        'style': true,
+      }
+    ]),]
 })
